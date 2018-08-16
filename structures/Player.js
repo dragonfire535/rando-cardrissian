3e9ab8be95bb733bbbd3d9e5cc1807f307c299ef
Dragon Fire <danielbodendahl@gmail.com>
2018-08-16 18:00:17 -0400

    Fix deal being there when it shouldn't be

--- structures/Player.js
+++ structures/Player.js
@@ -26,7 +26,7 @@ module.exports = class Player {
 
 	async turn(channel, czar, black, deck, chosenCards) {
 		if (this.user.id === czar.user.id) return;
-		if (this.hand.size < 10) this.hand.add(deck.deal());
+		if (this.hand.size < 10) this.hand.add(deck.draw());
 		try {
 			if (this.hand.size < black.pick) {
 				await this.user.send('You don\'t have enough cards!');
