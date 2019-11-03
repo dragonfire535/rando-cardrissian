28aae3aebc53473ccb2329fe6b010707b1daca83
Dragon Fire <danielbodendahl@gmail.com>
2019-11-02 23:52:18 -0400

    Allow days in uptime

--- commands/util/info.js
+++ commands/util/info.js
@@ -27,7 +27,7 @@ module.exports = class InfoCommand extends Command {
 			.addField('❯ Source Code',
 				source ? `[Here](https://github.com/${RANDO_GITHUB_REPO_USERNAME}/${RANDO_GITHUB_REPO_NAME})` : 'N/A', true)
 			.addField('❯ Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
-			.addField('❯ Uptime', moment.duration(this.client.uptime).format('hh:mm:ss', { trim: false }), true)
+			.addField('❯ Uptime', moment.duration(this.client.uptime).format('d:hh:mm:ss'), true)
 			.addField('❯ Version', `v${version}`, true)
 			.addField('❯ Node Version', process.version, true)
 			.addField('❯ Dependencies', this.parseDependencies());
