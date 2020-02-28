e3b980d0197c1a6c853cae1e78f31c71ef163b09
Dragon Fire <danielbodendahl@gmail.com>
2020-02-28 12:25:26 -0500

    Fix lint

--- commands/util/info.js
+++ commands/util/info.js
@@ -38,7 +38,7 @@ module.exports = class InfoCommand extends Command {
 		return Object.entries(deps).map(dep => {
 			if (dep[1].startsWith('github:')) {
 				const repo = dep[1].replace('github:', '').split('/');
-				return `[${dep[0]}](https://github.com/${repo[0]}/${repo[1].replace(/#(.+)/, '/tree/$1')`;
+				return `[${dep[0]}](https://github.com/${repo[0]}/${repo[1].replace(/#(.+)/, '/tree/$1')})`;
 			}
 			return `[${dep[0]}](https://npmjs.com/${dep[0]})`;
 		}).join(', ');
